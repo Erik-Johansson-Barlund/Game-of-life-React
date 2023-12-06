b5baa2e9a6fd0c872ff6229637e2039e41d2e710
Erik Johansson Bärlund <erik.johansson-barlund@inter.ikea.com>
2023-12-06 22:46:09 +0100

    deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from "vite"
 
 export default defineConfig({
   plugins: [react()],
+  base: "/Game-of-life-React/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
